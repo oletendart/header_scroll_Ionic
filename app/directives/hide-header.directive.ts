ecbdb3e0381b3a9e8ffc531e8bf8f835edcd74b1
oletendart <letendartolivia@gmail.com>
2020-10-23 12:45:55 +0200

    Create contructor contains private renderer and private domCtrl in the file hide-header.directive.ts

--- app/directives/hide-header.directive.ts
+++ app/directives/hide-header.directive.ts
@@ -1,11 +1,14 @@
-import { Directive, OnInit } from "@angular/core";
+import { Directive, OnInit, Input, Renderer2 } from "@angular/core";
+import { DomController } from "ionic-angular";
 
 @Directive({
   selector: "[appHideHeader]"
 })
 export class HideHeaderDirective implements OnInit {
   @Input("appHideHeader") toolbar: any;
-  ngOnInit(): void {
-    throw new Error("Method not implementd.");
+  private toolbarHeight = 44;
+
+  constructor(private renderer: Renderer2, private domCtrl: DomController) {
+    
   }
 }
