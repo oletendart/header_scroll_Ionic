30742a3d110c401d4b988adf12e93bbc215f266d
oletendart <letendartolivia@gmail.com>
2020-10-23 12:28:41 +0200

    Modificate home.module.ts => Import components and module

--- app/home/home.module.ts
+++ app/home/home.module.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { IonicModule } from "@ionic/angular";
+import { FormsModule } from "@angular/forms";
+import { HomePage } from "./home.page";
 
-@Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+import { HomePageRoutingModule } from "./home-routing.module";
+
+@NgModule({
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  declarations: [HomePage]
 })
-export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
-}
+export class HomePageModule {}
