8ed5352a81f8838a0f2c96cc8df00ed404257034
oletendart <letendartolivia@gmail.com>
2020-10-23 12:19:08 +0200

    Import NgModule, CommonModule and Hide/Fade directive in the file share-directives.module.ts

---
+++ app/directives/share-directives.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HideHeaderDirective } from "./hide-header.directive";
+import { FadeHeaderDirective } from "./fade-header.directive";
+
+@NgModule({
+  declarations: [HideHeaderDirective, FadeHeaderDirective],
+  imports: [CommonModule]
+})
+export class SharedDirectivesModule {}
