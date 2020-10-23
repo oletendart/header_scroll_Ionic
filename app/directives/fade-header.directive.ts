97648a29ae0771e464e1d85cc069edfc0c5f6685
oletendart <letendartolivia@gmail.com>
2020-10-23 13:23:12 +0200

    Import components + c/c the hide in the fade function in the file fade-header.directive.ts

---
+++ app/directives/fade-header.directive.ts
@@ -0,0 +1,41 @@
+import {
+  Directive,
+  OnInit,
+  Input,
+  Renderer2,
+  HostListener
+} from "@angular/core";
+import { DomController } from "@ionic/angular";
+
+@Directive({
+  selector: "[appaFadeHeader]"
+})
+export class FadeHeaderDirective implements OnInit {
+  @Input("appHideHeader") toolbar: any;
+  private toolbarHeight = 44;
+
+  constructor(private renderer: Renderer2, private domCtrl: DomController) {}
+
+  ngOnInit() {
+    this.toolbar = this.toolbar.el;
+    this.domCtrl.read(() => {
+      this.toolbarHeight = this.toolbar.clientHeight;
+    });
+  }
+
+  @HostListener("ionScroll", ["$event"]) onContentScroll($event) {
+    const scrollTop = $event.detail.scrollTop;
+    let newPosition = -(scrollTop / 5);
+
+    if (newPosition < -this.toolbarHeight) {
+      newPosition = -this.toolbarHeight;
+    }
+
+    let newOpacity = 1 - newPosition / -this.toolbarHeight;
+
+    this.domCtrl.write(() => {
+      this.renderer.setStyle(this.toolbar, "top", `${newPosition}px`);
+      this.renderer.setStyle(this.toolbar, "opacity", newOpacity);
+    });
+  }
+}
