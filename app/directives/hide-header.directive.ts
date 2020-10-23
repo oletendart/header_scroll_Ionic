912ee718d859c402ca3f202f2b64511d0720af70
oletendart <letendartolivia@gmail.com>
2020-10-23 12:49:24 +0200

    Create toolbar in the ngOnInit + console.log test the function in the file hide-header.directive.ts

--- app/directives/hide-header.directive.ts
+++ app/directives/hide-header.directive.ts
@@ -11,6 +11,8 @@ export class HideHeaderDirective implements OnInit {
   constructor(private renderer: Renderer2, private domCtrl: DomController) {}
 
   ngOnInit() {
+    console.log("Test:", this.toolbar);
+    this.toolbar = this.toolbar.el;
     this.domCtrl.read(() => {
       this.toolbarHeight = this.toolbar.clientHeight;
     });
